30bb1abe4fbd26e3e3dd55a2a6ea095619ffc3e0
Jonathan Pucm <jonathancoder77@outlook.com>
2017-09-04 12:15:15 +1000

    Style about page and link to resources

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -5,8 +5,17 @@ var React = require('react');
 var About = (props) => {
   return (
     <div>
-    <h3>About</h3>
-    <p> Welcome to the about page </p>
+    <h1 className="text-center">About</h1>
+    <p> This is an application built on React. This application which allows users to track the weather, was built to familiarise myself and practice with React.</p>
+    <p>Check out my source code: <a href="https://github.com/jonathanpuc/ReactWeather" target="_blank">GitHub repository</a></p>
+    <p>Here are some of the tools I used:</p>
+    <ul>
+      <li><p><a href="https://facebook.github.io/react/" target="_blank">React</a></p></li>
+      <li><p><a href="https://nodejs.org/en/" target="_blank">Node.js</a></p></li>
+      <li><p><a href="https://expressjs.com/" target="_blank">Express.js</a></p></li>
+      <li><p><a href="http://foundation.zurb.com/" target="_blank">Foundation</a></p></li>
+      <li><p><a href="https://openweathermap.org/" target="_blank">Open Weather Map</a></p></li>
+    </ul>
     </div>
   )
 };
