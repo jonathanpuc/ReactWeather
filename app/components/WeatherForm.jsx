8224df82696fca0d14a83404f9a94174ff78b0ea
Jonathan Pucm <jonathancoder77@outlook.com>
2017-09-04 14:59:21 +1000

    Add error modal

--- app/components/WeatherForm.jsx
+++ app/components/WeatherForm.jsx
@@ -16,7 +16,7 @@ var WeatherForm = React.createClass({
     return (
 
       <form onSubmit={this.onFormSubmit}>
-        <input type="text" placeholder="Enter city name" ref="location"/>
+        <input type="text" placeholder="Enter a location, e.g Melbourne, Australia" ref="location"/>
         <button className="button expanded hollow primary">Get weather</button>
       </form>
 
