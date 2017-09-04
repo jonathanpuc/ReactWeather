89bf23542dc23856affeefb4191237e68a4b9736
Jonathan Pucm <jonathancoder77@outlook.com>
2017-09-04 16:26:16 +1000

    Add styling to nav

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -36,10 +36,10 @@ var Nav = React.createClass({
           <form onSubmit={this.onSearch}>
             <ul className="menu">
               <li>
-                <input type="search" placeholder="Search weather by city" ref="search"/>
+                <input type="search" placeholder="Search weather by location" ref="search"/>
               </li>
               <li>
-                <input type="submit" className="button" value="Get Weather"/>
+                <input type="submit" className="button" value="Get weather"/>
               </li>
             </ul>
           </form>
